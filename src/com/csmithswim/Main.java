d18feedeacc38a04e78ff4d9a7545f1f48b19d2f
Cullen S <cullensmi@gmail.com>
2020-12-20 17:20:58 -0500

    solved algorithm

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,8 +1,21 @@
 package com.csmithswim;
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try {
+            int intS = Integer.parseInt(S);
+            System.out.println(intS);
+        } catch(Exception e){
+            System.out.println("Bad String");
+        }
     }
 }
